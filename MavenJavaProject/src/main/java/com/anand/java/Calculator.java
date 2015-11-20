06e3f36285ae26f029d903a7569f29e3285df6a1
anandkushwaha <anandkushwahakiet@gmail.com>
2015-11-21 01:38:37 +0530

    Added Calculater interface and classes

---
+++ MavenJavaProject/src/main/java/com/anand/java/Calculator.java
@@ -0,0 +1,9 @@
+package com.anand.java;
+
+public interface Calculator {
+
+	int add(int ...add);
+	int multiply(int ...add);
+	int power(int base, int power);
+	
+}
---
+++ MavenJavaProject/src/main/java/com/anand/java/CalculatorImpl.java
@@ -0,0 +1,20 @@
+package com.anand.java;
+
+public class CalculatorImpl implements Calculator {
+
+	public int add(int... add) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int multiply(int... add) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int power(int base, int power) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
